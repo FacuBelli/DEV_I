cdba2b85e9fc0047ea7dc9c45ea33f2913084c59
FacuBelli <facundo_carabelli@hotmail.com>
2025-04-14 19:36:16 -0300

    Inicio de page Empecemos

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,21 +1,84 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+// app/(tabs)/home.tsx
+import { View, Text, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
 
 export default function HomeScreen() {
+  const router = useRouter();
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-    </View>
+    <ImageBackground
+      source={require('../../assets/images/Logo1.jpg')}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View style={styles.overlay} />
+
+      <View style={styles.content}>
+        <Text style={styles.logo}>Casiimote</Text>
+
+        <View style={styles.centerTextContainer}>
+          <Text style={styles.title}>Comparte tu sabor</Text>
+          <Text style={styles.title}>y descubre el mundo</Text>
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={() => router.push('/explore')}>
+          <Text style={styles.buttonText}>Empecemos</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.loginText}>
+          ¿Ya tenes usuario? <Text style={styles.loginLink}>Inicia sesión</Text>
+        </Text>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'flex-end',
   },
-  text: {
-    fontSize: 20,
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0,0,0,0.3)',
+  },
+  content: {
+    padding: 20,
+    paddingBottom: 50,
+  },
+  logo: {
+    color: '#B05FFF',
+    fontSize: 28,
+    fontWeight: 'bold',
+    position: 'absolute',
+    top: 50,
+    left: 20,
+  },
+  centerTextContainer: {
+    marginBottom: 30,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: '#B05FFF',
+    paddingVertical: 12,
+    borderRadius: 30,
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  loginText: {
+    textAlign: 'center',
+    color: '#fff',
+  },
+  loginLink: {
+    color: '#FFD800',
+    fontWeight: 'bold',
   },
 });
